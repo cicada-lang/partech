7c36ab0af4f8716d3d034d66449190da04eca753
Xie Yuheng <xyheme@gmail.com>
2021-09-16 19:53:44 +0800

    check_parentheses

--- src/lexers/common.ts
+++ src/lexers/common.ts
@@ -8,8 +8,43 @@ const table_lexer = TableLexer.build([
   ["symbol", "^\\s*([^_\\p{Letter}0-9\\s])\\s*", "u"],
 ])
 
+type Result =
+  | { kind: "balance" }
+  | { kind: "lack"; token: Token.Token }
+  | { kind: "excess"; token: Token.Token }
+  | { kind: "mismatch"; token: Token.Token }
+
+function check_parentheses(tokens: Array<Token.Token>): Result {
+  const stack: Array<Token.Token> = []
+  for (const token of tokens) {
+    if (
+      token.label === "symbol" &&
+      ["(", ")", "[", "]", "{", "}"].contains(token.value)
+    ) {
+      if (["(", "[", "{"].contains(token.value)) {
+        stack.push(token)
+      } else {
+        const top = stack.pop()
+        if (top === undefined) {
+          return { kind: "excess", token }
+        } else if (token.value !== top.value) {
+          return { kind: "mismatch", token }
+        }
+      }
+    }
+  }
+
+  if (stack.length === 0) {
+    return { kind: "balance" }
+  } else {
+    return { kind: "lack", token: stack.pop() }
+  }
+}
+
 export const common = {
   lex(text: string): Array<Token.Token> {
     return text.trim().length === 0 ? [] : table_lexer.lex(text)
   },
+
+  check_parentheses,
 }
