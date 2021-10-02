c9de4676e215a88d63574bd444541e54b406152b
Xie Yuheng <xyheme@gmail.com>
2021-10-02 23:42:53 +0800

    up

--- src/errors/parsing-error.ts
+++ src/errors/parsing-error.ts
@@ -10,4 +10,18 @@ export class ParsingError extends BaseError {
     this.message = message
     this.span = opts.span
   }
+
+  get concise_message(): string {
+    const lines = this.message.split("\n")
+    if (lines.length > 7) {
+      return [
+        // NOTE Show first 7 lines, for first three entres.
+        // TODO This should be improved.
+        ...lines.slice(0, 7),
+        " and so on ...",
+      ].join("\n")
+    } else {
+      return this.message
+    }
+  }
 }
