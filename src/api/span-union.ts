8d94fe580379d8684e5ef8da7e6b5c663e4e53df
Xie Yuheng <xyheme@gmail.com>
2021-10-04 19:14:11 +0800

    up

--- src/api/span-union.ts
+++ src/api/span-union.ts
@@ -1,6 +1,6 @@
 import * as Span from "../span"
 
-export function span_union(spans: Array<Span.Span>): Span.Span {
+export function span_closure(spans: Array<Span.Span>): Span.Span {
   const lo = Math.min(...spans.map((span) => span.lo))
   const hi = Math.max(...spans.map((span) => span.hi))
   return { lo, hi }
