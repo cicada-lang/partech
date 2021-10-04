f9c053a484ed4f1d7f223de9ce4c5f46fca091d5
Xie Yuheng <xyheme@gmail.com>
2021-10-04 19:20:04 +0800

    up

--- src/api/span-union.ts
+++ src/api/span-union.ts
@@ -1,6 +1,12 @@
 import * as Span from "../span"
 
 export function span_closure(spans: Array<Span.Span>): Span.Span {
+  if (spans.length === 0) {
+    throw new Error(
+      "I can not calculate the closure of empty array of spans, at least one span is needed."
+    )
+  }
+
   const lo = Math.min(...spans.map((span) => span.lo))
   const hi = Math.max(...spans.map((span) => span.hi))
   return { lo, hi }
