9920073da50af15a3edcb9504afc8fc724778190
Xie Yuheng <xyheme@gmail.com>
2022-10-29 14:46:33 +0800

    dashline.test.ts

--- src/tests/dashline.test.ts
+++ src/tests/dashline.test.ts
@@ -19,5 +19,61 @@ const parse = pt.gen_parse({
 })
 
 test("line of more then one dashes", () => {
-  expect
+  expect(
+    parse(`
+---
+---
+---
+`),
+  ).toMatchInlineSnapshot(`
+    {
+      "body": {},
+      "head": {
+        "kind": "dashline",
+        "name": "dashline",
+      },
+      "kind": "Tree.node",
+      "span": {
+        "hi": 12,
+        "lo": 1,
+      },
+    }
+`)
+
+  expect(parse("---")).toMatchInlineSnapshot(`
+    {
+      "body": {},
+      "head": {
+        "kind": "dashline",
+        "name": "dashline",
+      },
+      "kind": "Tree.node",
+      "span": {
+        "hi": 3,
+        "lo": 0,
+      },
+    }
+`)
+
+  expect(parse("--")).toMatchInlineSnapshot(`
+    {
+      "body": {},
+      "head": {
+        "kind": "dashline",
+        "name": "dashline",
+      },
+      "kind": "Tree.node",
+      "span": {
+        "hi": 2,
+        "lo": 0,
+      },
+    }
+  `)
+
+  expect(() => parse("-")).toThrowErrorMatchingInlineSnapshot(`
+    "Found END_OF_TOKENS, while expecting:
+     \\"-\\":
+         dashline:dashline@0 -> [1m[31m> [39m[22m\\"-\\" one_or_more
+    "
+  `)
 })
