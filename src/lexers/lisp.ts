f9e80f5afa771a49a841a47e7405a9f0f46b450c
Xie Yuheng <xyheme@gmail.com>
2022-12-05 07:55:48 +0800

    up

--- src/lexers/lisp.ts
+++ src/lexers/lisp.ts
@@ -4,16 +4,6 @@ import * as Token from "../token"
 // NOTE Remind that all regular exps use `^` which means beginning.
 const table_lexer = TableLexer.build([
   ["identifier", "^\\s*([-_\\p{Letter}][-_\\p{Letter}0-9]*)\\s*", "u"],
-  [
-    "identifier_starts_with_uppercase",
-    "^\\s*([A-Z][_\\p{Letter}0-9]*)\\s*",
-    "u",
-  ],
-  [
-    "identifier_starts_with_lowercase",
-    "^\\s*([_a-z][_\\p{Letter}0-9]*)\\s*",
-    "u",
-  ],
   ["string", '^\\s*("(\\\\.|[^"])*")\\s*'],
   ["number", "^\\s*(\\d+\\.\\d+|\\d+|-\\d+\\.\\d+|-\\d+)\\s*"],
   ["symbol", "^\\s*([^-_\\p{Letter}0-9\\s])\\s*", "u"],
